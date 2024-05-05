d7c537b4e07f87b50bdf707731be1c3daea0e570
Sarthak S Kumar <sarthakskumar7@gmail.com>
2024-05-05 20:57:36 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   images: {
-    formats: ["image/avif", "image/webp", "image/png", "image/jpg"],
+    formats: ["image/avif", "image/webp"],
     remotePatterns: [
       {
         protocol: "https",
