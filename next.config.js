8c23c62ce6eccee0946e835fcb5be916a5eb8bc6
Sarthak S Kumar <sarthakskumar7@gmail.com>
2024-05-17 10:50:14 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,14 @@
 
+module.exports = {
+  images: {
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/diqvllts4/**",
+      },
+    ],
+  },
+};
